5c63932ecb062c3ec7f566c27d6fa5e9116b2d5c
João Pedro <106753611+jpcofer@users.noreply.github.com>
2024-10-01 22:22:17 -0300

    Update common.js

--- graficos/common.js
+++ graficos/common.js
@@ -1,21 +1,31 @@
 
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
 
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
 
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
-
-    const container = document.getElementById('graficos-container')
+    paragrafo.innerHTML = texto
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
